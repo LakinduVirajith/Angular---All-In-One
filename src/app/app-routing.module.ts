b08883ecc9b0a9804bb9f426e6f69cf0c69f9a32
Lakindu <it21066184@my.sliit.lk>
2023-07-24 14:58:20 +0530

    structure changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { DataBindingComponent } from './binding-methods/data-binding/data-binding.component';
-import { EventBindingComponent } from './binding-methods/event-binding/event-binding.component';
-import { TwoWayBindingComponent } from './binding-methods/two-way-binding/two-way-binding.component';
 
 import { IPostComponent } from './decorators/input-decorator/i-post/i-post.component';
 import { UPostComponent } from './decorators/output-decorator/u-post/u-post.component';
+
+import { DataBindingComponent } from './binding-methods/data-binding/data-binding.component';
+import { EventBindingComponent } from './binding-methods/event-binding/event-binding.component';
+import { TwoWayBindingComponent } from './binding-methods/two-way-binding/two-way-binding.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
 
+  { path: 'input-decorator', component: IPostComponent },
+  { path: 'output-decorator', component: UPostComponent },
+
   { path: 'data-binding', component: DataBindingComponent },
   { path: 'event-binding', component: EventBindingComponent },
   { path: 'two-way-binding', component: TwoWayBindingComponent },
-
-  { path: 'input-decorator', component: IPostComponent },
-  { path: 'output-decorator', component: UPostComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,29 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { DataBindingComponent } from './binding-methods/data-binding/data-binding.component';
-import { EventBindingComponent } from './binding-methods/event-binding/event-binding.component';
-import { TwoWayBindingComponent } from './binding-methods/two-way-binding/two-way-binding.component';
 
 import { IPostComponent } from './decorators/input-decorator/i-post/i-post.component';
 import { IPostListComponent } from './decorators/input-decorator/i-post-list/i-post-list.component';
 import { UPostComponent } from './decorators/output-decorator/u-post/u-post.component';
 import { UPostListComponent } from './decorators/output-decorator/u-post-list/u-post-list.component';
 
+import { DataBindingComponent } from './binding-methods/data-binding/data-binding.component';
+import { EventBindingComponent } from './binding-methods/event-binding/event-binding.component';
+import { TwoWayBindingComponent } from './binding-methods/two-way-binding/two-way-binding.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DataBindingComponent,
-    EventBindingComponent,
-    TwoWayBindingComponent,
+
     IPostComponent,
     IPostListComponent,
     UPostComponent,
     UPostListComponent,
+
+    DataBindingComponent,
+    EventBindingComponent,
+    TwoWayBindingComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
