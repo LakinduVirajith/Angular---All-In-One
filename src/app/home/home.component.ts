0d2504b1d71b750674abe3c866908ba06ea40a95
Lakindu <it21066184@my.sliit.lk>
2023-07-25 13:29:16 +0530

    feature needed functions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,10 @@ export class HomeComponent {
   onClick(option: number) {
     this.option = option;
 
-    if (option == 1 || 2 || 3 || 4) this.pageHeader = '';
+    if (option == 1) this.pageHeader = 'Angular Decorators';
+    if (option == 2) this.pageHeader = 'Angular Bindings';
+    if (option == 3) this.pageHeader = 'Angular Directives';
+    if (option == 4) this.pageHeader = 'Angular Forms';
     if (option == 0) this.pageHeader = 'Angular All In One';
   }
 }
